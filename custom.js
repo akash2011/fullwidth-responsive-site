ac78ae20a9633b3922b45e671ce6f550d41ba5d2
Ashraful Kabir <ashraful_info@yahoo.com>
2019-01-10 12:36:17 -0500

    custom js updated

--- custom.js
+++ custom.js
@@ -2,10 +2,32 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
       navigation: true,
       navigationPosition: 'right',
-      navigationTooltips: ['section1', 'section2','section3','section4',],
+      navigationTooltips: ['Home', 'About','section3','Expericens','Blog','Contact'],
       showActiveTooltip: true,
       slidesNavigation: true,
         slidesNavPosition: 'bottom',
       controlArrows:false,
     });
+    /* End of full page */
+
+    var showHeaderAt = 150;
+		var win = $(window),
+				body = $('body');
+		// Show the fixed header only on larger screen devices
+	
+			// When we scroll more than 150px down, we set the
+			// "fixed" class on the body element.
+			win.on('scroll', function(e){
+        alert('on scroll...');
+				if(win.scrollTop() > showHeaderAt) {
+					body.addClass('fixed');
+				}
+				else {
+					body.removeClass('fixed');
+				}
+			});
+    
+    /* End of fixed header menu */
+
+
 });
